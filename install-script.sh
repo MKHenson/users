426e9cb6154a9992e6c5a43fb3606368906c064d
MKHenson <mat@webinate.net>
2015-06-04 11:48:21 +0100

    Updated shell script

--- install-script.sh
+++ install-script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
-echo "hello world"
+{ # this ensures the entire script is downloaded #
+
+# Prints the latest stable version
+nvm_latest_version() {
+  echo "v0.0.1"
+}
+
+echo "hello world $(nvm_latest_version)"
 exit
+} # this ensures the entire script is downloaded #
