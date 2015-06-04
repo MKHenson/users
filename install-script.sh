7562f45d1c56729920ff30b2cbce82742c915952
MKHenson <mat@webinate.net>
2015-06-04 12:52:23 +0100

    Updated shell script

--- install-script.sh
+++ install-script.sh
@@ -9,12 +9,18 @@ nvm_latest_version() {
   echo "v0.0.1"
 }
 
+GREEN='\032[0;31m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 echo "Downloading latest version from github $(nvm_latest_version)"
 
 #download latest
 wget https://github.com/MKHenson/webinate-users/archive/master.zip
 unzip -j "master.zip" "webinate-users-master/server/*"
 rm master.zip
-echo "Users successfully installed"
+copy "example-config.json" "config.json"
+echo "${GREEN}Users successfully installed ${NC}"
+echo "${GREEN}Please run an NPM update and edit the config.json ${NC}"
 exit
 } # this ensures the entire script is downloaded #
