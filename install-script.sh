4f649fbf6902c4a5e7b03470a00ff2a990498180
MKHenson <mat@webinate.net>
2015-06-04 12:17:13 +0100

    Updated shell script

--- install-script.sh
+++ install-script.sh
@@ -6,6 +6,14 @@ nvm_latest_version() {
   echo "v0.0.1"
 }
 
-echo "hello world $(nvm_latest_version)"
+echo "Downloading latest version from github $(nvm_latest_version)"
+
+#download latest
+wget https://github.com/MKHenson/webinate-users/archive/master.zip
+unzip -j “master.zip” “webinate-users-master/server/*”
+rm master.zip
+npm update
+	
+echo "Users successfully installed"
 exit
 } # this ensures the entire script is downloaded #
