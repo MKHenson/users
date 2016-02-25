8377ea78b3416ad1258543cac674cef1481ae98a
Mathew Henson <mat@webinate.net>
2016-02-25 16:29:24 +0000

    Added gulp and typescript files to get ready for vs code

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+// CONFIG
+// ==============================
+var outDir = "server";
+
+// Builds each of the ts files into JS files in the output folder
+gulp.task('ts-code', function() {
+
+    return gulp.src(['references.d.ts', 'lib/**/*.ts', 'lib/**/*.json'], { base: "lib" })
+        .pipe(ts({
+            "module": "commonjs",
+            "removeComments": false,
+            "noEmitOnError": true,
+            "declaration": false,
+            "sourceMap": false,
+            "preserveConstEnums": true,
+            "target": "es5",
+            "noImplicitAny": false
+            }))
+        .pipe(gulp.dest(outDir));
+});
+
+gulp.task('build-all', ['ts-code']);
