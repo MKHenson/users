8ca05bdb3714b5dbc155df4c4947603e83f54a8c
Mathew Henson <mat@webinate.net>
2016-02-29 17:15:22 +0000

    Removed unused definition files and created definition folder for required and custom definition files

--- gulpfile.js
+++ gulpfile.js
@@ -22,4 +22,22 @@ gulp.task('ts-code', function() {
         .pipe(gulp.dest(outDir));
 });
 
-gulp.task('build-all', ['ts-code']);
+// Builds each of the ts files into JS files in the output folder
+gulp.task('ts-code-definitions', function() {
+
+    return gulp.src(['references.d.ts', 'src/**/*.ts', 'src/**/*.json'], { base: "src/lib" })
+        .pipe(ts({
+            "module": "commonjs",
+            "removeComments": false,
+            "noEmitOnError": true,
+            "declaration": true,
+            "sourceMap": false,
+            "preserveConstEnums": true,
+            "target": "es5",
+            "noImplicitAny": false,
+            "outFile":"definition.d.ts"
+            })).dts
+        .pipe(gulp.dest(outDir + "/definitions"));
+});
+
+gulp.task('build-all', ['ts-code', 'ts-code-definitions']);
