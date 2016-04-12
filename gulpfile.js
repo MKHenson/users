88386c0063068d92268f4eadf66f1ab95c5bdcb8
Mathew Henson <mat@webinate.net>
2016-04-12 17:18:50 +0100

    Updated gulp file to use tsconfig properties

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,35 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var fs = require('fs');
 
 // CONFIG
 // ==============================
-var outDir = "dist";
+var tsConfig = JSON.parse(fs.readFileSync('tsconfig.json'));
 
 // Builds each of the ts files into JS files in the output folder
 gulp.task('ts-code', function() {
 
     return gulp.src(['src/**/*.ts', 'src/**/*.json'], { base: "src" })
         .pipe(ts({
-            "module": "commonjs",
-            "removeComments": false,
-            "noEmitOnError": true,
-            "declaration": false,
-            "sourceMap": false,
-            "preserveConstEnums": true,
-            "target": "es5",
-            "noImplicitAny": false
+            "module": tsConfig.compilerOptions.module,
+            "removeComments": tsConfig.compilerOptions.removeComments,
+            "noEmitOnError": tsConfig.compilerOptions.noEmitOnError,
+            "declaration": tsConfig.compilerOptions.declaration,
+            "sourceMap": tsConfig.compilerOptions.sourceMap,
+            "preserveConstEnums": tsConfig.compilerOptions.preserveConstEnums,
+            "target": tsConfig.compilerOptions.target,
+            "noImplicitAny": tsConfig.compilerOptions.noImplicitAny,
+            "allowUnreachableCode": tsConfig.compilerOptions.allowUnreachableCode,
+            "allowUnusedLabels": tsConfig.compilerOptions.allowUnusedLabels
             }))
-        .pipe(gulp.dest(outDir));
+        .pipe(gulp.dest(tsConfig.compilerOptions.outDir));
 });
 
 // Builds each of the ts files into JS files in the output folder
 gulp.task('ts-code-definitions', function() {
 
     return gulp.src(['src/definitions/custom/definitions.d.ts'], { base: "src/definitions/custom/" })
-        .pipe(gulp.dest(outDir + "/definitions"));
+        .pipe(gulp.dest(tsConfig.compilerOptions.outDir + "/definitions"));
 });
 
 gulp.task('build-all', ['ts-code', 'ts-code-definitions']);
