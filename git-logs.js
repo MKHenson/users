2321f2de2101ec0e063905a60f0782de9de27ad5
Mathew Henson <mat@webinate.net>
2017-03-08 13:38:06 +0000

    Updated logs

--- git-logs.js
+++ git-logs.js
@@ -3,8 +3,8 @@ var fs = require( 'fs' )
 var exec = require( 'child_process' ).exec;
 var child;
 
-var prevTag = "v0.3.3";
-var nextTag = "v0.4.0";
+var prevTag = "v0.4.0";
+var nextTag = "v0.5.0";
 
 
 // Executes the git log command
